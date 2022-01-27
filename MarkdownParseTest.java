b8a777f1588001d996db2eb75d11be59241dfcc5
AlexRussell4 <awrussell@ucsd.edu>
2022-01-26 17:29:27 -0800

    added tests

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,31 @@
 import static org.junit.Assert.*; //imports assert
+
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.junit.*; //imports junit
 
 public class MarkdownParseTest { //class declaration
     @Test //identifies a Test for junit
     public void addition() { //this is the method that is run for the test
         assertEquals(2, 1 + 1); //asserting that 2 = 1 + 1
+    }
+
+    @Test 
+    public void testGetLinks() throws IOException,NoSuchFileException {
+        Path fileName = Path.of("test-file.md");
+        Path fileName2 = Path.of("new-file2.md");
+        Path fileName3 = Path.of("new-file3.md");
+        Path fileName4 = Path.of("new-file4.md");
+
+
+        assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(fileName)));
+        //assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(fileName2)));
+        //assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(fileName3)));
+        //assertEquals(List.of("https://something.com", "some-page.html"), MarkdownParse.getLinks(Files.readString(fileName4)));
     }
 }
 
