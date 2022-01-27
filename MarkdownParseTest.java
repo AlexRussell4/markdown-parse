aa2169a27ac05ad5d0b0c9cdb651723d8858a9c7
AlexRussell4 <awrussell@ucsd.edu>
2022-01-26 16:39:29 -0800

    added comments

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,10 +1,10 @@
-import static org.junit.Assert.*;
-import org.junit.*;
+import static org.junit.Assert.*; //imports assert
+import org.junit.*; //imports junit
 
-public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(2, 1 + 1);
+public class MarkdownParseTest { //class declaration
+    @Test //identifies a Test for junit
+    public void addition() { //this is the method that is run for the test
+        assertEquals(2, 1 + 1); //asserting that 2 = 1 + 1
     }
 }
 
